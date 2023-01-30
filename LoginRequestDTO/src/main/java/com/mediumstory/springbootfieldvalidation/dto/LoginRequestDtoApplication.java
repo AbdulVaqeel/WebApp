f3431a2d1fe6329c236d2ff3f2899c337412693b
AbdulVaqeel <118118448+AbdulVaqeel@users.noreply.github.com>
2023-01-30 14:04:26 +0530

    LoginRequestDTO

---
+++ LoginRequestDTO/src/main/java/com/mediumstory/springbootfieldvalidation/dto/LoginRequestDtoApplication.java
@@ -0,0 +1,13 @@
+package com.mediumstory.springbootfieldvalidation.dto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginRequestDtoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginRequestDtoApplication.class, args);
+	}
+
+}
---
+++ LoginRequestDTO/src/test/java/com/mediumstory/springbootfieldvalidation/dto/LoginRequestDtoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mediumstory.springbootfieldvalidation.dto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginRequestDtoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
